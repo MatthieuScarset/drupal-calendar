9dc817fce9d7955a4f375bca4b4c4d67fc56b70a
Matthieu SCARSET <m@matthieuscarset.com>
2023-11-13 15:19:06 +0100

    feat: Cleanup script

---
+++ scripts/clean_codebase.sh
@@ -0,0 +1,16 @@
+# Delete Drupal test directories.
+# https://unix.stackexchange.com/a/89937
+find . -type d -name tests -exec rm -rf {} +
+# Delete testing and demo profiles.
+find web/core/profiles -type d -name 'testing*' -exec rm -rf {} +
+find web/core/profiles -type d -name 'nightwatch_testing' -exec rm -rf {} +
+find web/core/profiles -type d -name 'demo_umami' -exec rm -rf {} +
+# Delete README files.
+find . -type f -name 'README*' -delete
+find . -type f -name 'CHANGELOG*' -delete
+# Delete patch records.
+find . -type f -name 'PATCHES.txt' -delete
+# Delete markdown files except licenses.
+find web -type f -name "*.md" -not -name "LICENSE*" -delete
+# Delete txt files except licenses.
+find web -type f -name "*.txt" -not -name "humans*" -not -name "LICENSE*" -not -name "COPYRIGHT*" -delete
